8442adf4c10abfe1ad881c12b406a8284168f637
DanielBagdasarian <88898635+DanielBagdasarian@users.noreply.github.com>
2024-03-12 19:31:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,18 @@ from .. import loader, utils
 
 @loader.tds
 class MyModule(loader.Module):
-    """My module"""
+    """My module test"""
     strings = {
         "name": "MyModuleTest",
         "hello": "Hello world!"
     }
 
+    @loader.command(
+        ru_doc="Привет мир!",
+        es_doc="¡Hola mundo!",
+        de_doc="Hallo Welt!",
+        # ...
+    )
     async def test(self, message: Message):
         """Hello world test"""
-        await utils.answer(message, strings["hello"])
+        await utils.answer(message, self.strings("hello"))
