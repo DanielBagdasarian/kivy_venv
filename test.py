3e754db5c9dd8b87a93c3e191f527fc729c010ed
DanielBagdasarian <88898635+DanielBagdasarian@users.noreply.github.com>
2024-03-12 19:32:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 from hikkatl.types import Message
-from .. import loader, utils
+from .. import loader
 
 
 @loader.tds
 class MyModule(loader.Module):
-    """My module test"""
+    """My module"""
     strings = {
         "name": "MyModuleTest",
         "hello": "Hello world!"
     }
+    strings_ru = {"hello": "Привет мир!"}
+    strings_es = {"hello": "¡Hola mundo!"}
+    strings_de = {"hello": "Hallo Welt!"}
 
     @loader.command(
         ru_doc="Привет мир!",
@@ -16,6 +19,6 @@ class MyModule(loader.Module):
         de_doc="Hallo Welt!",
         # ...
     )
-    async def test(self, message: Message):
-        """Hello world test"""
+    async def helloworld(self, message: Message):
+        """Hello world"""
         await utils.answer(message, self.strings("hello"))
